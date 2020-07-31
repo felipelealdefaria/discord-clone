6399592e8a039ea2ef67d9c6214d4ad093b5f012
felipelealdefaria <felipelealdefaria@gmail.com>
2020-07-31 18:04:03 -0300

    task: create ChannelInfo component

---
+++ src/components/ChannelInfo/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, HashtagIcon, Separator, Title, Description } from './styles'
+
+const ChannelInfo: React.FC = () => {
+  return (
+    <Container>
+      <HashtagIcon />
+      <Title>chat-livre</Title>
+      <Separator />
+      <Description>
+        Canal aberto para conversas
+      </Description>
+    </Container>
+  )
+}
+
+export default ChannelInfo
